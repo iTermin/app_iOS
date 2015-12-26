4bbef2eecd3b28db31352268712c3c039f00ed66
Estefania Chavez Guardado <fachinacg@gmail.com>
2015-12-26 14:55:22 +0100

    Initial Commit

---
+++ MeetingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/26/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
