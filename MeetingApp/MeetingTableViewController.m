d8b3de6eb4c3f55f78636281b797308ab2a11606
Luis Rangel <luis.sanchez@liquidlabs.de>
2015-12-27 15:01:13 +0100

    Minimum cell data

--- MeetingApp/MeetingTableViewController.m
+++ MeetingApp/MeetingTableViewController.m
@@ -8,15 +8,41 @@
 
 #import "MeetingTableViewController.h"
 
+
 @interface MeetingTableViewController ()
 
 @end
 
 @implementation MeetingTableViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSArray *meetings = @[
+            @{
+                    @"active" : @(true),
+                    @"date" : @(1448008691974),
+                    @"meetingId" : @"m1",
+                    @"name" : @"Meeting 1"
+            }];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell * cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"IDENTIFIER"];
+    [cell setBackgroundColor: indexPath.row == 0 ? [UIColor redColor] : [UIColor greenColor] ];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
