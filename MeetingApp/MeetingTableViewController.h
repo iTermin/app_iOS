93be654ae9cde471aaa180968db544d1444d2ee9
Luis Rangel <luis.sanchez@liquidlabs.de>
2015-12-27 14:31:54 +0100

    Rename main view controller to MeetingTableViewController

---
+++ MeetingApp/MeetingTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/26/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeetingTableViewController : UITableViewController
+
+
+@end
+
---
+++ MeetingApp/MeetingTableViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/26/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import "MeetingTableViewController.h"
+
+@interface MeetingTableViewController ()
+
+@end
+
+@implementation MeetingTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
