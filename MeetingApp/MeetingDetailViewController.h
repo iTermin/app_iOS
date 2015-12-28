f15b3619963592550d464b44d82f4647d9255984
Estefania Chavez Guardado <fachinacg@gmail.com>
2015-12-28 09:47:57 +0100

    Add MeetingDetailViewController

---
+++ MeetingApp/MeetingDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeetingDetailViewController.h
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/28/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeetingDetailViewController : UIViewController
+
+@end
