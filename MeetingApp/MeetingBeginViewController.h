f9dc2aeb62cb8d4558449c1c9c165af66cae0a5a
Estefania Chavez Guardado <fachinacg@gmail.com>
2015-12-28 09:24:15 +0100

    Add files of MeetingBeginViewController

---
+++ MeetingApp/MeetingBeginViewController.h
@@ -0,0 +1,15 @@
+//
+//  MeetingBeginViewController.h
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/28/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+/* MeetingBeginViewController_h */
+
+#import <UIKit/UIKit.h>
+
+@interface MeetingBeginViewController : UIViewController
+
+
+@end
---
+++ MeetingApp/MeetingBeginViewController.m
@@ -0,0 +1,29 @@
+//
+//  MeetingBeginViewController.m
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 12/28/15.
+//  Copyright © 2015 Estefania Chavez Guardado. All rights reserved.
+//
+
+
+#import "MeetingBeginViewController.h"
+
+
+@interface MeetingBeginViewController ()
+
+@end
+
+@implementation MeetingBeginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
