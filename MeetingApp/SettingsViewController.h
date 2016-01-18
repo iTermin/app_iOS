4c5d9ca3742ff83090932af0805e041d7c738f34
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-01-18 11:15:03 +0100

    Add files

---
+++ MeetingApp/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MeetingApp
+//
+//  Created by Estefania Chavez Guardado on 1/18/16.
+//  Copyright © 2016 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
