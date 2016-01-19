0f98400c14f7d8b9eb755969073ba58e7b3ced2c
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-01-19 15:16:36 +0100

    Points requiered for the tableViewController

--- MeetingApp/SettingsViewController.m
+++ MeetingApp/SettingsViewController.m
@@ -10,4 +10,47 @@
 
 @implementation SettingsViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.meetings = @[
+                      @{
+                          @"active" : @(true),
+                          @"email" : @"correo@",
+                          @"name" : @"Estefania"
+                          },
+                      ];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell * cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"IDENTIFIER"];
+    cell.textLabel.font = [UIFont fontWithName:@"BodoniSvtyTwoITCTT-Bold" size:15];
+    cell.detailTextLabel.font = [UIFont fontWithName:@"BodoniSvtyTwoITCTT-Book" size:11];
+    [[cell textLabel] setText: self.meetings[indexPath.row][@"name"]];
+    [[cell detailTextLabel] setText: self.meetings[indexPath.row][@"email"] ];
+    [cell setAccessoryType: UITableViewCellAccessoryDisclosureIndicator];
+    
+    return cell;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
 @end
