ed73a283f0b7b21c8eb1eb4325a92d2a8bee1030
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-02-29 16:20:14 +0100

    Prepare the inputs for the algorithm

--- MeetingApp/Meeting/MeetingDateSelectorViewController.m
+++ MeetingApp/Meeting/MeetingDateSelectorViewController.m
@@ -8,6 +8,7 @@
 
 #import "UXDateCellsManager.h"
 #import "MeetingDateSelectorViewController.h"
+#import "ArrayOfCountries.h"
 
 @interface MeetingDateSelectorViewController ()
 
@@ -24,7 +25,7 @@
     
     // Set up the initial values for the date cells manager
     NSDate *startDate = [NSDate date]; //now
-    NSDate *endDate = [NSDate dateWithTimeIntervalSinceNow:60 * 60 * 24 * 7]; //one week from now
+    NSDate *endDate = [NSDate dateWithTimeIntervalSinceNow:7200]; //one week from now
     NSIndexPath *allDayCellIndexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     NSIndexPath *startDateCellIndexPath = [NSIndexPath indexPathForRow:1 inSection:0];
     NSIndexPath *endDateCellIndexPath = [NSIndexPath indexPathForRow:2 inSection:0];
@@ -56,11 +57,19 @@
     }];
     
     self.tableView.tableFooterView = [[UIView alloc]initWithFrame:CGRectZero];
+    
+    self.arrayCountries = [ArrayOfCountries new];
+    self.modelCountries = [self.arrayCountries getModelCountries];
+    self.dateCurrent = [NSDate new];
+    self.dateCurrent = startDate;
+    
+    self.hoursArray = [NSMutableArray array];
+    
+    // TODO :[self inputAlgoritm:startDate];
 
 }
 
 - (void)viewWillAppear:(BOOL)animated{
-    
     //[self updateViewModel];
 }
 
@@ -85,11 +94,79 @@
 {
     // Date Cells
     if ([self.dateCellsManager isManagedDateCell:indexPath]) {
+        self.dateCurrent = self.dateCellsManager.startDate;
+        [self inputAlgoritm:self.dateCurrent];
         return [self.dateCellsManager tableView:tableView willSelectRowAtIndexPath:indexPath];
     }
     
     // Other cells
     return indexPath;
+}
+
+-(void) inputAlgoritm: (NSDate *) startDate {
+    
+    NSDictionary * userDate = [self getHourOfDate:startDate];
+    [self.hoursArray addObject:userDate[@"hour"]];
+    
+    [self.viewModel enumerateObjectsUsingBlock:^(NSDictionary * guest, NSUInteger idx, BOOL * stop) {
+        NSDictionary *dataGuest = guest[@"data"];
+        [self gethourGuest: dataGuest[@"codeCountry"] respectUser:userDate];
+    }];
+}
+
+- (void) gethourGuest: (NSString *) guestCountry respectUser: (NSDictionary *) userDate{
+    NSArray * UTCGuest  = [self getUTCGuest: guestCountry];
+    NSArray * UTCUser = [self getUTCGuest: userDate[@"countryCode"]];
+    if(![UTCUser isEqualToArray:UTCGuest]){
+
+        [UTCUser enumerateObjectsUsingBlock:^(id hour, NSUInteger idx, BOOL * stop) {
+            [self addDiferencial:[hour doubleValue] ToGuest:UTCGuest withCurrentHours:[userDate[@"hour"] doubleValue]];
+        }];
+        
+    } else {
+        [self.hoursArray addObject:userDate[@"hour"]];
+    }
+}
+
+-(NSArray *) getUTCGuest: (NSString *) codeCountry{
+    __block NSArray *UTCGuest = [NSArray array];
+    
+    [self.modelCountries enumerateObjectsUsingBlock:^(NSDictionary * country, NSUInteger idx, BOOL * stop) {
+        if([country[@"code"] isEqualToString:codeCountry])
+            UTCGuest = [NSArray arrayWithArray:country[@"UTC"]];
+    }];
+    return UTCGuest;
+}
+
+- (void) addDiferencial: (double) UTC_hourUser ToGuest: (NSArray *) UTC_country withCurrentHours: (double) currentHours{
+    [UTC_country enumerateObjectsUsingBlock:^(id hour, NSUInteger idx, BOOL * stop) {
+        
+        double temporalHour = UTC_hourUser < 0 ?
+        (UTC_hourUser + [hour doubleValue]) + currentHours :
+        (UTC_hourUser - [hour doubleValue]) + currentHours;
+        
+        temporalHour = temporalHour <= 0 ? temporalHour + 24.0 : temporalHour;
+        temporalHour = temporalHour > 24 ? temporalHour - 24 : temporalHour;
+        
+        NSNumber *hourGuest = [NSNumber numberWithDouble:(temporalHour)];
+        [self.hoursArray addObject: hourGuest];
+    }];
+}
+
+- (NSDictionary *) getHourOfDate: (NSDate *) startDate{
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    [dateFormatter setDateFormat:@"HH"];
+    int hour = [[dateFormatter stringFromDate:startDate] intValue];
+    [dateFormatter setDateFormat:@"mm"];
+    int minutes = [[dateFormatter stringFromDate:startDate] intValue];
+    
+    return @{ @"hour" : @(hour), @"minutes" : @(minutes), @"countryCode" : [self getCountryUser]};
+}
+
+-(NSString *) getCountryUser {
+    NSLocale *currentLocale = [NSLocale currentLocale];  // get the current locale.
+    NSString *countryCode = [currentLocale objectForKey:NSLocaleCountryCode];
+    return countryCode;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
