e2d45064acba419f3ea83a51f4884d69ae53cc6e
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-04-08 11:07:39 +0200

    Fix the show image of the guest in detailMeeting

--- MeetingApp/Meeting/Artifacts/GuestViewCell.m
+++ MeetingApp/Meeting/Artifacts/GuestViewCell.m
@@ -27,15 +27,47 @@
     [self.nameGuest setText: data[@"name"]];
     [self.emailGuest setText: data[@"email"]];
     
-    if ([data[@"photo"] isEqualToString:@""]) {
-        NSString *nameGuest = data[@"name"];
-        [self.guestImage setImageWithString:nameGuest color:nil circular:YES];
-    } else{
+    if ([data[@"photo"] isKindOfClass:[NSString class]]){
+        if ([data[@"photo"] isEqualToString:@""]) {
+            NSString *nameGuest = data[@"name"];
+            [self.guestImage setImageWithString:nameGuest color:nil circular:YES];
+        } else{
+            [self.guestImage setImage:circularImageWithImage
+             ([UIImage imageWithData: [self decodeBase64ToImage:_data[@"photo"]]])];
+        }
+    } else {
         self.guestImage.layer.cornerRadius = self.guestImage.frame.size.width/2.0f;
         self.guestImage.clipsToBounds = YES;
-        NSString *identyPhoto = [NSString stringWithFormat:@"%@.png", data[@"photo"]];
-        [self.guestImage setImage:[UIImage imageNamed:identyPhoto]];
+        [self.guestImage setImage:data[@"photo"]];
     }
+}
+
+static UIImage *circularImageWithImage(UIImage *inputImage)
+{
+    
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 20, 160, 160)];
+    
+    // Create an image context containing the original UIImage.
+    UIGraphicsBeginImageContext(inputImage.size);
+    
+    // Clip to the bezier path and clear that portion of the image.
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextAddPath(context,bezierPath.CGPath);
+    CGContextClip(context);
+    
+    // Draw here when the context is clipped
+    [inputImage drawAtPoint:CGPointZero];
+    
+    // Build a new UIImage from the image context.
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (NSData *)decodeBase64ToImage:(NSString *)strEncodeData {
+    NSData *data = [[NSData alloc]initWithBase64EncodedString:strEncodeData options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    return data;
 }
 
 @end
